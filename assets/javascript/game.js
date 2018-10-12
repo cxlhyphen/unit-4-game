ffcada5ff1d7d67924be7e01f8ce5201c30d59d7
cxlhyphen <42073246+cxlhyphen@users.noreply.github.com>
2018-10-12 02:23:49 -0400

    finished most of game, pre-final-tweaks

---
+++ assets/javascript/game.js
@@ -0,0 +1,77 @@
+//Global Variables
+var scoreToWin;
+var currScore;
+
+var wins = 0;
+var losses = 0;
+
+var silver = 0;
+var gold = 0;
+var red = 0;
+var blue = 0;
+
+//Functions
+var randomNumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function gameStart() {
+    currScore = 0;
+    scoreToWin = randomNumber(19, 120);
+
+    silver = randomNumber(1, 12);
+    gold = randomNumber(1, 12);
+    red = randomNumber(1, 12);
+    blue = randomNumber(1, 12);
+
+    console.log("current: " + currScore + " to win  " + scoreToWin);
+    console.log("silver:" + silver + " gold:" + gold + " red:" + red + " blue:" + blue);
+
+    //Grab IDs & update
+    $("#scoreToWin").text(scoreToWin);
+    $("#score").text(currScore);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+
+}
+
+function compareScores() {
+
+    $("#score").text(currScore);
+
+    if (currScore === scoreToWin) {
+        wins++;
+        alert("You Win!");
+        gameStart();
+    }
+
+    else if (currScore >= scoreToWin) {
+        losses++;
+        alert ("You Lose!");
+        gameStart();
+    }
+}
+
+$("#silver").on("click", function() {
+    currScore = currScore += silver;
+    compareScores();
+    
+});
+
+$("#gold").on("click", function() {
+    currScore = currScore += gold;
+    compareScores();
+});
+
+$("#red").on("click", function() {
+    currScore = currScore += red;
+    compareScores();
+});
+
+$("#blue").on("click", function() {
+    currScore = currScore += blue;
+    compareScores();
+});
+
+//Start Game
+gameStart();
